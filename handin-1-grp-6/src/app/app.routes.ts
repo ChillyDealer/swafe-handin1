eeb9ea1e6554a742754432eb1aae1fa83a029661
ChillyDealer <49028947+ChillyDealer@users.noreply.github.com>
2025-09-15 14:51:56 +0200

    sigma navbar

--- handin-1-grp-6/src/app/app.routes.ts
+++ handin-1-grp-6/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AddCreditCard } from './pages/add-credit-card/add-credit-card';
+import { Transactions } from './pages/transactions/transactions';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'add-card', component: AddCreditCard },
+  { path: 'transactions', component: Transactions },
+  { path: '**', redirectTo: '' }
+];
