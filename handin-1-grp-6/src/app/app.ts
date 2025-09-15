c1bddc88942d926bffb9e09d7b520e836ab5082d
ChillyDealer <49028947+ChillyDealer@users.noreply.github.com>
2025-09-15 15:14:12 +0200

    httpclient

--- handin-1-grp-6/src/app/app.ts
+++ handin-1-grp-6/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './nav-bar/nav-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
