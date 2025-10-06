e2b0786df5e07a0f6cbeb3d81b819957b54a96b6
OliverEC <oliverec04@gmail.com>
2025-10-06 14:01:25 +0200

    form field

--- handin-1-grp-6/src/app/form-field/form-field.ts
+++ handin-1-grp-6/src/app/form-field/form-field.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {ControlValueAccessor} from '@angular/forms';
 
 @Component({
   selector: 'app-form-field',
@@ -6,6 +7,19 @@ import {Component, Input} from '@angular/core';
   templateUrl: './form-field.html',
   styleUrl: './form-field.css'
 })
-export class FormField {
+export class FormField implements ControlValueAccessor{
   @Input({required: true}) label!: string;
+
+  writeValue(obj: any): void {
+      throw new Error("Method not implemented.");
+  }
+  registerOnChange(fn: any): void {
+      throw new Error("Method not implemented.");
+  }
+  registerOnTouched(fn: any): void {
+      throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+      throw new Error("Method not implemented.");
+  }
 }
